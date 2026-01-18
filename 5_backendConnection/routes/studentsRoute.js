5faee5d3945b23d869482f6bc812fffb23552e0a
satyampal <em.satyam@gmail.com>
2026-01-19 00:23:42 +0400

    Express Js routes, all operations

---
+++ 5_backendConnection/routes/studentsRoute.js
@@ -0,0 +1,64 @@
+const express = require('express');
+
+const router = express.Router();
+
+const student = require('../moduls/SchoolStudents');
+
+
+// Push the Student Data to the Database
+router.post('/', async(req, res)=>{
+    try{
+        const data = req.body;
+
+        const studedentData = new student(data);
+
+        const response = await studedentData.save();
+        
+        console.log('menueItem => data saved successfully')
+        res.status(200).json({response}); // this will show the status in the network section show there that all the data the is fielled and the post to the server
+
+
+    }
+    catch(error){
+           console.log('Failed to save data');
+           res.status(500).json({error : 'Internal Server Error'})
+    };
+    
+});
+
+// Get the student data from the database
+router.get('/', async(req, res)=>{
+    try{
+        const data = await student.find(); 
+
+        console.log('data fetched successfully');
+
+      res.status(200).json(data);
+
+    }catch(error){
+           console.log('Failed to save data');
+           res.status(500).json({error : 'Internal Server Error'})
+    };
+});
+
+// genderType data get from the database
+router.get('/:gender', async(req, res)=> {
+    try{
+        const genderType = req.params.gender ;  // :gender this variable is here; extract the work type from the gender parameter
+    if(genderType =='male' || genderType == 'female' || genderType == 'other'){
+
+        const response = await student.find({gender:genderType});
+
+        console.log('Response Fetched');
+        res.status(200).json({response});
+
+    }else{
+        res.status(404).json({error : 'Invalid Work Type'});
+    }
+        }catch(error){
+           console.log('Failed to save data');
+           res.status(500).json({error : 'Internal Server Error'})
+    };
+})
+
+module.exports = router;
